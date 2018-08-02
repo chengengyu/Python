3a51e16bfc75ced05993eb292a9d110ee05e4397
eling <cgy008@outlook.com>
2018-08-02 22:40:27 +0800

    支持多个提交

--- autoweb/autoweb.py
+++ autoweb/autoweb.py
@@ -1,30 +1,45 @@
 from splinter import Browser
 import datetime
 
-dateConfig = [\
+htmlDir = r'K:\\work\\GitHub\\Python\\autoweb\\会议室预定系统.html'
+
+config1 = [\
     {\
         'room': '702',\
         'startTime': '10:30 AM',\
         'endTime': '11:30 AM',\
         'text': 'tl'\
     },\
-    {},\
+    {\
+        'room': '701',\
+        'startTime': '9:30 AM',\
+        'endTime': '10:30 AM',\
+        'text': 'lh'\
+    }\
+]
+
+config2 = []
+config3 = [\
     {\
         'room': '702',\
         'startTime': '10:30 AM',\
         'endTime': '11:30 AM',\
         'text': 'lh'\
-    },\
-    {},\
+    }\
+]
+config4 = []
+config5 = [\
     {\
         'room': '702',\
         'startTime': '10:30 AM',\
         'endTime': '11:30 AM',\
         'text': 'pmr'\
-    },\
-    {},\
-    {}
+    }\
 ]
+config6 = []
+config7 = []
+
+dateConfig = [config1, config2, config3, config1, config5, config6, config7]
 
 '''
 	<option value="701">A402室</option>
@@ -51,13 +66,13 @@ dateConfig = [\
 
 def getDate():
     today = datetime.datetime.now()
-    detal = datetime.timedelta(days=5)
+    detal = datetime.timedelta(days=7)
     roomDay = today + detal
     return roomDay
 
 def setHtml(roomDay, config):
     b = Browser(driver_name='chrome')
-    url = r'K:\\work\\GitHub\\Python\\autoweb\\会议室预定系统.html'
+    url = htmlDir
     b.visit(url)
     b.select('selLocation', config['room'])
     b.fill('textDescription', config['text'])
@@ -65,14 +80,15 @@ def setHtml(roomDay, config):
     b.select('selStartDay', roomDay.day)
     b.select('selStartTime', config['startTime'])
     b.select('selEndTime', config['endTime'])
-    b.click_link_by_id('submit')
+    #b.click_link_by_id('submit')
 
 if __name__ =="__main__":
     roomDay = getDate()
-    config = dateConfig[roomDay.weekday()]
-    if config:
-        print(config)
-        setHtml(roomDay, config)
+    configList = dateConfig[roomDay.weekday()]
+    if configList:
+        print(configList)
+        for config in configList:
+            setHtml(roomDay, config)
         input("wait")
     else:
         print("null")
